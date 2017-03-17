6db5f29fabfdfe7cea3a2bba6fb90c4def54c947
Yoland Gao <yoland5286@gmail.com>
2017-03-17 19:51:40 -0400

    Calculated PIDs

--- src/pages/pid/dataprocess.ts
+++ src/pages/pid/dataprocess.ts
@@ -21,7 +21,7 @@ export class PidDataProcess{
     return parseInt(data.replace(" ", "").trim(), 16);
   }
 
-  public static getData(pid: string, data: string, unit: string, iUnit?: string, iUnitConvert?: any) : string{
+  public static getData(pid: string, data: string) : string{
     let func;
     switch(pid){
      case "010C": func = this._010C; break;
@@ -29,10 +29,6 @@ export class PidDataProcess{
      default: func = this.defaultFunc; break;
     }
 
-    if(PidDataProcess.useImperialUnits && iUnitConvert != null){
-      return String(iUnitConvert(func(data.substring(6))) + iUnit);
-    }else{
-      return String(func(data.substring(6)) + unit);
-    }
+    return func(data.substring(6));
   }
 }
--- src/pages/pid/pid.ts
+++ src/pages/pid/pid.ts
@@ -14,7 +14,10 @@ import { PidDataProcess } from './dataprocess';
 export class PidPage {
 
   private sensors = [];
+  private static sensorsCache = [];
   private static timer = null;
+  private static init: boolean = false;
+
 
   constructor(public navCtrl: NavController, private storage: Storage) {
     if(!Bluetooth.adapterInit){
@@ -22,51 +25,102 @@ export class PidPage {
       return;
     }
 
-    BLE.isConnected(Bluetooth.uuid).then(() => {
-      this.pushSensor("010C", "GENERAL", "Vehicle RPM", "rpm");
-      this.pushSensor("0110", "GENERAL", "Mass Air Flow", "g/sec");
-      this.pushSensor("010D", "GENERAL", "Vehicle Speed", "km/h", "mph", kph => {return 0.621371 * kph});
-    }).catch(() => {
-      HomePage.bleError(navCtrl, storage);
-    });
+    if(!PidPage.init){
+      BLE.isConnected(Bluetooth.uuid).then(() => {
+        this.pushSensor("010C", "GENERAL", "Vehicle RPM", "rpm");
+        this.pushSensor("0110", "GENERAL", "Mass Air Flow", "g/sec");
+        this.pushSensor("010D", "GENERAL", "Vehicle Speed", "km/h", "mph", kph => {return 0.621371 * kph});
+
+        this.pushSensor("_MPG", "GENERAL", "Fuel Economy", "kml", "mpg", kml => {return 2.35215 * kml}, (pid, obj, sensor) => {
+          obj.updateSensor(pid, obj.appendUnits(10, sensor));
+        });
+
+        PidPage.init = true;
+      }).catch(() => {
+        HomePage.bleError(navCtrl, storage);
+      });
+    }else{
+      this.sensors = PidPage.sensorsCache;
+    }
 
     PidPage.timer = setInterval(() => {
       BLE.isConnected(Bluetooth.uuid).then(() => {
         for(let i = 0 ; i < this.sensors.length; i++){
-          this.updateSensor(this.sensors[i].pid);
+          this.update(this.sensors[i].pid);
         }
       }).catch(() => {
         HomePage.bleError(navCtrl, storage);
       });
     }, 1000);
+
   }
 
-  pushSensor(pid: string, category: string, name: string, unit: string, iUnit?: string, iUnitFunction?: any){
-    Bluetooth.writeToUUID(pid + "\r").then(data => {
-      if(!data.includes("NO_DATA")){
-        let item = {pid: pid, name: name, value: "NO DATA", category: category, unit: unit};
-        if(iUnit != null){
-          item["iUnit"] = iUnit;
-          item["iUnitFunction"] = iUnitFunction;
+  pushSensor(pid: string, category: string, name: string, unit: string, iUnit?: string, iUnitFunction?: any, updateFunction?: any){
+    if(pid.includes("_")){
+      this.pushSensorIntoArray(pid, category, name, unit, iUnit, iUnitFunction, updateFunction);
+    }else{
+      Bluetooth.writeToUUID(pid + "\r").then(data => {
+        if(!data.includes("NO_DATA")){
+          this.pushSensorIntoArray(pid, category, name, unit, iUnit, iUnitFunction, updateFunction);
         }
-        this.sensors.push(item);
-      }
-    })
+      })
+    }
   }
 
-  updateSensor(pid: string){
+  pushSensorIntoArray(pid: string, category: string, name: string, unit: string, iUnit?: string, iUnitFunction?: any, updateFunction?: any){
+    let item = {pid: pid, name: name, value: "NO DATA", category: category, unit: unit, updateFunction: null};
+    if(iUnit != null){
+      item["iUnit"] = iUnit;
+      item["iUnitFunction"] = iUnitFunction;
+    }
+    item["updateFunction"] = updateFunction != null ? updateFunction : this.bluetoothUpdateFunction;
+    this.sensors.push(item);
+    PidPage.sensorsCache.push(item);
+  }
+
+  update(pid: string){
+    for(let i = 0; i < this.sensors.length; i++){
+      if(this.sensors[i].pid === pid){
+        console.log("For PID: " + pid + " the update function is: " + this.sensors[i].updateFunction);
+        this.sensors[i].updateFunction(pid, this, this.sensors[i]);
+      }
+    }
+  }
+
+  appendUnits(data, sensor){
+    let iUnitConvert = sensor.iUnitFunction;
+    let iUnit = sensor.iUnit;
+    let unit = sensor.unit;
+
+    if(PidDataProcess.useImperialUnits && iUnitConvert != null){
+      return String(data + iUnit);
+    }else{
+      return String(data + unit);
+    }
+  }
+
+  updateSensor(pid: string, data){
+    for(let i = 0; i < this.sensors.length; i++){
+      if(this.sensors[i].pid === pid){
+        this.sensors[i].value = data;
+      }
+    }
+  }
+
+  //DO NOT USE this. functions, it will fail!
+  bluetoothUpdateFunction(pid, obj, sensor){
     Bluetooth.writeToUUID(pid + "\r").then(data => {
       if(!data.includes("NO_DATA")){
-        for(let i = 0; i < this.sensors.length; i++){
-          if(this.sensors[i].pid === pid){
-            this.sensors[i].value = PidDataProcess.getData(pid, data, this.sensors[i].unit, this.sensors[i].iUnit, this.sensors[i].iUnitFunction);
+        for(let i = 0; i < obj.sensors.length; i++){
+          if(obj.sensors[i].pid === pid){
+            obj.updateSensor(pid, obj.appendUnits(PidDataProcess.getData(pid, data), obj.sensors[i]));
           }
         }
       }
     });
   }
 
-  ionViewDidLeave(){
+  ionViewWillLeave(){
     clearInterval(PidPage.timer);
   }
 
