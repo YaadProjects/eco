0de5f9f582e6bfd4e8d5f1b6137f360f33aae726
Yoland Gao <yoland5286@gmail.com>
2017-04-01 16:16:27 -0400

    Drawing user path

--- src/pages/trip/trip.ts
+++ src/pages/trip/trip.ts
@@ -15,6 +15,9 @@ export class TripPage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  marker: any;
+  path: any;
+  coords: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController) {}
 
@@ -28,25 +31,47 @@ export class TripPage {
         let marker;
         let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         if(!this.hasMapLoaded){
-          let mapOptions = {
-            center: latLng,
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true,
-            streetViewControl: false
-          }
-          this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-          marker = new google.maps.Marker({
-            position: latLng,
-            map: this.map,
-          });
+          this.loadMap(latLng);
         }
-        marker.position = latLng;
+        this.marker.setPosition(latLng);
+        this.map.setCenter(latLng);
+        this.coords.push({lat: position.coords.latitude, lng: position.coords.longitude});
+
         console.log("Location: " + position.coords.latitude + ' ' + position.coords.longitude);
         this.location[0].value = String(position.coords.latitude);
         this.location[1].value = String(position.coords.longitude);
       }
     });
+  }
+
+  loadMap(latLng: any){
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
+      streetViewControl: false
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 5,
+        strokeColor: '#81D4FA'
+      }
+    });
+    this.coords = [];
+    this.path = new google.maps.Polyline({
+      path: this.coords,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+    this.path.setMap(this.map);
+    this.hasMapLoaded = true;
   }
 
   ionViewDidLeave(){
